b1fee289168fb635eb79993874f08ba063032a89
Hope Hutman <hhutman@telefonprojekt.com>
2019-04-11 13:06:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import SubmissionForm from './SubmissionForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h2>What is the most important issue facing the United States today?</h2>
+          <SubmissionForm />
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/SubmissionForm.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+
+function SubmissionForm(){
+  return (
+    <h2>This is a submission form</h2>
+  )
+}
+
+
+// class submissionForm extends Component {
+//   render() {
+//     return (
+//       <div className="submissionForm">
+//
+//
+//       </div>
+//     );
+//   }
+// }
+export default SubmissionForm
