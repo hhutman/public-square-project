d2ac0b9b53564b609208df724ba0c11299fa362e
Hope Hutman <hhutman@telefonprojekt.com>
2019-04-15 18:52:07 -0400

    styling form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 import SubmissionForm from './SubmissionForm'
-import Images from './public/Images'
+import Img from 'react-image'
+
 
 class App extends Component {
   render() {
@@ -13,18 +14,15 @@ class App extends Component {
           <span className="strike-center">Public Square Project</span>
           </div>
 
-
-
-             <img src={Images} />
-
-
-
+          <Img src="working_mother.png" />
 
           <div className="TopicIntro">
           Quality child care is expensive. In many states, it can cost more than tuition at a public university. It’s also in scarce supply.  The average cost of child care in the United States can range from 9 to 36 percent of a family’s income, depending on where they live. Moody’s estimates that the typical family paying for child care spends about 10 percent of their income on it, but the challenge is especially acute for low-income families and single parents. In some states, a minimum wage worker simply would need to work more weeks than exist in a year to afford an average-priced day care.
 
           </div>
+          <div className="Submission_Form">
           <SubmissionForm />
+          </div>
       </div>
     );
   }
--- src/SubmissionForm.js
+++ src/SubmissionForm.js
@@ -1,115 +1,63 @@
 import React from "react"
 import ReactDom from "react-dom"
 
+const inputStyle = {
+  width: '300px',
+
+}
 
 class SubmissionForm extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: "",
-            lastName: "",
-            isFriendly: false,
-            gender: "",
-            favColor: "blue"
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
 
-    handleChange(event) {
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
-    }
+  constructor() {
+      super()
+      this.state = {
+          publicSquareName: "",
+          userEmail: "",
+          // isFriendly: false,
+          // gender: "",
+          // favColor: "blue"
+      }
+      this.handleChange = this.handleChange.bind(this)
+  }
 
-    render() {
-        return (
+  handleChange(event) {
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+  }
+
+  render() {
+      return (
             <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    value={this.state.firstName}
-                    name="firstName"
-                    placeholder="First Name"
-                    onChange={this.handleChange}
-                />
-                <br />
-                <input
-                    type="text"
-                    value={this.state.lastName}
-                    name="lastName"
-                    placeholder="Last Name"
-                    onChange={this.handleChange}
-                />
-
-                {
-                    /**
-                     * Other useful form elements:
-                     *
-                     *  <textarea /> element
-                     *  <input type="checkbox" />
-                     *  <input type="radio" />
-                     *  <select> and <option> elements
-                     */
-                }
-
-                <textarea
+              <input style={{inputStyle}}
                   type="text"
+                  value={this.state.publicSquareName}
+                  name="publicSquareName"
+                  placeholder="Choose Your Public Square Name"
+                  onChange={this.handleChange}
+              />
+
+              <input
+                  type="text"
+                  value={this.state.userEmail}
+                  name="userEmail"
+                  placeholder="Email: this will not be shown in the square"
+                  onChange={this.handleChange}
+              />
+
+              <textarea
+                  type= "text"
+                  value={this.state.blogText}
                   name="blogText"
-                    value={this.state.blogText}
-                    onChange={this.handleChange}
-                />
+                  placeholder="write your reply here"
+                  onChange={this.handleChange}
+              />
 
-                <br />
 
-                <label>
-                    <input
-                        type="checkbox"
-                        name="isFriendly"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Is friendly?
-                </label>
-                <br />
-                <label>
-                    <input
-                        type="radio"
-                        name="gender"
-                        value="male"
-                        checked={this.state.gender === "male"}
-                        onChange={this.handleChange}
-                    /> Male
-                </label>
-                <br />
-                <label>
-                    <input
-                        type="radio"
-                        name="gender"
-                        value="female"
-                        checked={this.state.gender === "female"}
-                        onChange={this.handleChange}
-                    /> Female
-                </label>
-                {/* Formik */}
-                <br />
 
-                <label>Favorite Color:</label>
-                <select
-                    value={this.state.favColor}
-                    onChange={this.handleChange}
-                    name="favColor"
-                >
-                    <option value="blue">Blue</option>
-                    <option value="green">Green</option>
-                    <option value="red">Red</option>
-                    <option value="orange">Orange</option>
-                    <option value="yellow">Yellow</option>
-                </select>
-
-                <h1>My nickname in the Public Square is:{this.state.firstName} {this.state.lastName}</h1>
-                <h2>You are a {this.state.gender}</h2>
-                <h2>The most important is is: {this.state.favColor}</h2>
-                <h2>Here's what I have to say: {this.state.blogText}</h2>
-                <button>Submit</button>
-            </form>
-        )
-    }
+              <button>Submit</button>
+          </form>
+      )
+  }
 }
+
 export default SubmissionForm
